3474666c2e12b3fe364054e48bcb7aa9fc112b25
MFSilva Tecnologia <johnny@etele.com.br>
2025-06-11 22:37:06 -0300

    Fix server for ES module environment

--- presentation-server.js
+++ presentation-server.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const { Server } = require('socket.io');
+import http from 'http';
+import { Server } from 'socket.io';
 
 const server = http.createServer();
 const io = new Server(server, {
